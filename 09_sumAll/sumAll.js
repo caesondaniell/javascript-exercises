90040a1ae2576b3c9c350659d3d72cd26a0e8202
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-17 14:49:47 -0800

    Complete JS exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (
+        a < 0 ||
+        b < 0 ||
+        Math.floor(a) !== a ||
+        Math.floor(b) !== b ||
+        isNaN(a) ||
+        isNaN(b)
+    ) return "ERROR";
+    const start = a < b ? a : b;
+    const end = a < b ? b : a;
+    let sum = 0;
+    for (let i = start; i <= end; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
