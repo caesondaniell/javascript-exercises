dc915f8960090f56018f383049af671fcf4cfc8f
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-17 15:39:11 -0800

    Make code more readable

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
     const leap = year % 100 === 0 && year % 400 === 0 ||
-    year % 4 === 0 && year % 100 !== 0;
+                year % 4 === 0 && year % 100 !== 0;
     return leap;
 };
 
