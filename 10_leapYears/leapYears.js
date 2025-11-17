8b0ee53d7e258090c198c12d15b0151be9fab09b
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-17 15:37:24 -0800

    Complete JS exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const leap = year % 100 === 0 && year % 400 === 0 ||
+    year % 4 === 0 && year % 100 !== 0;
+    return leap;
 };
 
 // Do not edit below this line
