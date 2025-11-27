aff54b3a9f0bab8132898afdc64822535b8ce749
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-27 09:07:10 -0800

    Complete JS exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, curr) => {
+    return acc += curr;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr) => {
+    return acc *= curr;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let val = num;
+	if (num === 0 || num === 1) return 1
+  else if (num > 1) {
+    for (let i = num - 1; i > 0; i--) {
+      val *= i;
+    }
+  }
+  return val;
 };
 
 // Do not edit below this line
