fa617eb61b9ab33ddd15b86330d4b48a703c6de5
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-27 10:01:00 -0800

    Complete JS exercise 13 poorly
    This works, but it's hideous. Now to find a
    better way.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const original = string
+    .split(' ')
+    .join('')
+    .split('.')
+    .join('')
+    .split(',')
+    .join('')
+    .split('?')
+    .join('')
+    .split('!')
+    .join('')
+    .split('-')
+    .join('')
+    .split(';')
+    .join('')
+    .split("'")
+    .join('')
+    .toLowerCase();
+    const reversed = original.split('').reverse().join('');
+    return original === reversed;
 };
 
 // Do not edit below this line
