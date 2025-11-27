dad6ab8e2932b06a2f261404d17e0ae58b00e7ac
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-27 11:53:51 -0800

    Complete JS exercise 14
    My initial solution works but I feel like I'm
    cheating by starting with the [1, 1] array.
    Will attempt to improve it.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const sequence = [1, 1];
+    const position = Number(input);
+    if (isNaN(position) || position < 0) return "OOPS"
+    else if (position === 0) return 0
+    else if (position > sequence.length - 1) {
+        for (let i = 2; i < position; i++) {
+          sequence.push(sequence[i - 1] + sequence[i - 2]);
+        }
+    }
+    return sequence[position - 1];
 };
 
 // Do not edit below this line
