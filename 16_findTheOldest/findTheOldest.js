9c479310fe3b7abd685a53404a5275f6d80e3be5
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-28 09:57:45 -0800

    Complete JS exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const present = new Date().getFullYear();
+    array.forEach(person => {
+        if (person.yearOfDeath) person["endYear"] = person.yearOfDeath
+        else person["endYear"] = present
+    });
+    array.sort((a, b) => (b.endYear - b.yearOfBirth) - (a.endYear - a.yearOfBirth));
+    return array[0];
 };
 
 // Do not edit below this line
